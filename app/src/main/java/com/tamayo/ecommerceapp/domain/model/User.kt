99b4ab284b6bc99119fba36ca5666b72ea84cc35
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-06-21 08:41:25 -0400

    Data class model

--- app/src/main/java/com/tamayo/ecommerceapp/domain/model/User.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/domain/model/User.kt
@@ -1,7 +1,24 @@
 package com.tamayo.ecommerceapp.domain.model
 
-data class User(
+import com.google.gson.annotations.SerializedName
 
+data class User(
+    @SerializedName("id")
     val id: String? = null,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("lastname")
+    val lastname: String,
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("phone")
+    val phone: String,
+    @SerializedName("password")
+    val password: String,
+    @SerializedName("image")
+    val image: String? = null,
+    @SerializedName("notification_token")
+    val notificationToke: String? = null,
+
 
 )
