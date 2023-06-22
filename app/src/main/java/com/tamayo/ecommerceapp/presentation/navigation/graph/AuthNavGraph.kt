ebd9d3fddaaf9dfb9d9e2ae535721a0ebc7cc86a
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-06-22 08:12:49 -0400

    AuthNavGraph.kt

--- app/src/main/java/com/tamayo/ecommerceapp/presentation/navigation/graph/AuthNavGraph.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/presentation/navigation/graph/AuthNavGraph.kt
@@ -9,19 +9,24 @@ import com.tamayo.ecommerceapp.presentation.navigation.Graph
 import com.tamayo.ecommerceapp.presentation.navigation.screen.AuthScreen
 import com.tamayo.ecommerceapp.presentation.screens.auth.login.LoginScreen
 import com.tamayo.ecommerceapp.presentation.screens.auth.register.RegisterScreen
+import com.tamayo.ecommerceapp.presentation.screens.home.HomeScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
-fun NavGraphBuilder.authNavGraph(navHostController: NavHostController){
+fun NavGraphBuilder.authNavGraph(navHostController: NavHostController) {
     navigation(
         route = Graph.AUTH,
         startDestination = AuthScreen.Login.route
-    ){
-        composable(route = AuthScreen.Login.route){
+    ) {
+        composable(route = AuthScreen.Login.route) {
             LoginScreen(navHostController)
         }
 
-        composable(route = AuthScreen.Register.route){
-          RegisterScreen(navHostController)
+        composable(route = AuthScreen.Register.route) {
+            RegisterScreen(navHostController)
+        }
+
+        composable(route = AuthScreen.Home.route) {
+            HomeScreen(navHostController = navHostController)
         }
     }
 }
