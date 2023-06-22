b774067b09fab667ccad5fe6f24d2effb6e1537e
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-06-22 08:13:12 -0400

    AuthRepositoryImpl

--- app/src/main/java/com/tamayo/ecommerceapp/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/data/repository/AuthRepositoryImpl.kt
@@ -2,19 +2,38 @@ package com.tamayo.ecommerceapp.data.repository
 
 import com.tamayo.ecommerceapp.data.repository.dataSource.AuthRemoteDataSource
 import com.tamayo.ecommerceapp.domain.model.AuthResponse
+import com.tamayo.ecommerceapp.domain.model.ErrorResponse
 import com.tamayo.ecommerceapp.domain.model.User
 import com.tamayo.ecommerceapp.domain.repository.AuthRepository
+import com.tamayo.ecommerceapp.domain.util.ConvertErrorBody
 import com.tamayo.ecommerceapp.domain.util.ResultState
+import retrofit2.HttpException
+import java.io.IOException
 
 class AuthRepositoryImpl(private val authRemoteDataSource: AuthRemoteDataSource) : AuthRepository {
     override suspend fun login(email: String, password: String): ResultState<AuthResponse> {
         return try {
             val result = authRemoteDataSource.login(email, password)
-            ResultState.Success(result.body()!!)
 
+            if (result.isSuccessful) { // 201
+                ResultState.Success(result.body()!!)
+
+            } else {
+                val errorResponse: ErrorResponse? =
+                    ConvertErrorBody.convertErrorBody(result.errorBody())
+                ResultState.Failure(errorResponse?.message ?: "Unexpected error")
+            }
+
+
+        } catch (e: HttpException) {
+            e.printStackTrace()
+            ResultState.Failure(e.message ?: "Http error request")
+        } catch (e: IOException) {
+            e.printStackTrace()
+            ResultState.Failure("Check your network connection")
         } catch (e: Exception) {
             e.printStackTrace()
-            ResultState.Failure(e)
+            ResultState.Failure(e.message ?: "Unexpected error")
         }
     }
 
