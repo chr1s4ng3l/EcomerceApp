0494302793f1a29dda23f4615c5664a5dbfdd5c3
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-06-28 11:18:27 -0400

    Register data source

--- app/src/main/java/com/tamayo/ecommerceapp/data/repository/dataSource/AuthRemoteDataSource.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/data/repository/dataSource/AuthRemoteDataSource.kt
@@ -7,4 +7,5 @@ import retrofit2.Response
 interface AuthRemoteDataSource {
 
     suspend fun login(email: String, password: String): Response<AuthResponse>
+    suspend fun register(user: User): Response<AuthResponse>
 }
--- app/src/main/java/com/tamayo/ecommerceapp/data/repository/dataSourceImpl/AuthRemoteDataSourceImpl.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/data/repository/dataSourceImpl/AuthRemoteDataSourceImpl.kt
@@ -2,10 +2,11 @@ package com.tamayo.ecommerceapp.data.repository.dataSourceImpl
 
 import com.tamayo.ecommerceapp.data.repository.dataSource.AuthRemoteDataSource
 import com.tamayo.ecommerceapp.data.service.AuthService
+import com.tamayo.ecommerceapp.domain.model.AuthResponse
 import com.tamayo.ecommerceapp.domain.model.User
 import retrofit2.Response
 
 class AuthRemoteDataSourceImpl(private val authService: AuthService) : AuthRemoteDataSource {
     override suspend fun login(email: String, password: String) = authService.login(email, password)
-
+    override suspend fun register(user: User): Response<AuthResponse> = authService.register(user)
 }
