b9c7fe47518f0ffa9478747b90b53cf8003a4cbd
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-07-07 13:29:44 -0400

    test1

--- app/src/main/java/com/tamayo/ecommerceapp/presentation/navigation/screen/AuthScreen.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/presentation/navigation/screen/AuthScreen.kt
@@ -2,7 +2,7 @@ package com.tamayo.ecommerceapp.presentation.navigation.screen
 
 sealed class AuthScreen(val route: String){
     object Login: AuthScreen("login")
-    object Register: AuthScreen("register")
+    object Register: AuthScreen("registerrr")
     object Home: AuthScreen("home")
     object Roles: AuthScreen("roles")
 }
