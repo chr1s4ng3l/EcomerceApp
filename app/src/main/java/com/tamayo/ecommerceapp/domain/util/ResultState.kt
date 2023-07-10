d9d5cc8fcf83b1ae6945f485a06ea9d99d5614ee
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-07-10 09:19:05 -0400

    Clean unneeded imports

--- app/src/main/java/com/tamayo/ecommerceapp/domain/util/ResultState.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/domain/util/ResultState.kt
@@ -1,7 +1,5 @@
 package com.tamayo.ecommerceapp.domain.util
 
-import java.lang.Exception
-
 sealed class ResultState<out T> {
     object Loading : ResultState<Nothing>()
     data class Success<out T>(val data: T) : ResultState<T>()
