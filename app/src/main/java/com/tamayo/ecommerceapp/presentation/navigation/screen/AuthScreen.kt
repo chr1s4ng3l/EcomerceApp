ae940ba173ba768dc3551af039ab2a32aa6d5d0f
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-07-10 13:54:05 -0400

    admin/home and client/home added

--- app/src/main/java/com/tamayo/ecommerceapp/presentation/navigation/screen/AuthScreen.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/presentation/navigation/screen/AuthScreen.kt
@@ -3,7 +3,7 @@ package com.tamayo.ecommerceapp.presentation.navigation.screen
 sealed class AuthScreen(val route: String){
     object Login: AuthScreen("login")
     object Register: AuthScreen("register")
-    object HomeClient: AuthScreen("homeClient")
-    object HomeAdmin: AuthScreen("homeAdmin")
+    object HomeClient: AuthScreen("client/home")
+    object HomeAdmin: AuthScreen("admin/home")
     object Roles: AuthScreen("roles")
 }
