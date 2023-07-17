fe41ad0f09c4483e06da6afdd699fc95e0c8a550
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-07-17 08:26:53 -0400

    Roles (Admin and Client) configured.

--- app/src/main/java/com/tamayo/ecommerceapp/presentation/navigation/graph/RolesNavGraph.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/presentation/navigation/graph/RolesNavGraph.kt
@@ -1,12 +1,12 @@
 package com.tamayo.ecommerceapp.presentation.navigation.graph
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.tamayo.ecommerceapp.presentation.navigation.Graph
 import com.tamayo.ecommerceapp.presentation.navigation.screen.RolesScreen
+import com.tamayo.ecommerceapp.presentation.screens.admin.home.AdminHomeScreen
 import com.tamayo.ecommerceapp.presentation.screens.client.home.ClientHomeScreen
 import com.tamayo.ecommerceapp.presentation.screens.roles.RolesScreen
 
@@ -21,6 +21,9 @@ fun NavGraphBuilder.rolesNavGraph(navHostController: NavHostController) {
 
         composable(route = Graph.CLIENT) {
             ClientHomeScreen()
+        }
+        composable(route = Graph.ADMIN) {
+            AdminHomeScreen()
         }
 
     }
