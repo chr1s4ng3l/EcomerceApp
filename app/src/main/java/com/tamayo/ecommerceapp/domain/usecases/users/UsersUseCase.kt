5a97d8f6aaacde514f8019a7c0d237ebb9a7758a
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-07-21 08:30:35 -0400

    UpdateWithImage was added

---
+++ app/src/main/java/com/tamayo/ecommerceapp/domain/usecases/users/UsersUseCase.kt
@@ -0,0 +1,6 @@
+package com.tamayo.ecommerceapp.domain.usecases.users
+
+data class UsersUseCase(
+    val updateUserUseCase: UpdateUserUseCase,
+    val updateUserWithImageUseCase: UpdateUserWithImageUseCase
+)
