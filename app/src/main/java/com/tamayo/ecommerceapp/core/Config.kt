f3adb7c6179624b02c6b0702db613ad83f25298c
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-07-25 08:54:42 -0400

    Backend route added

--- app/src/main/java/com/tamayo/ecommerceapp/core/Config.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/core/Config.kt
@@ -3,10 +3,12 @@ package com.tamayo.ecommerceapp.core
 object Config {
 
     //SERVICE API
-    //const val BASE_URL = "http://10.0.0.137:3000/"
-    const val BASE_URL = "http://192.168.1.85:3000/"
+    const val BASE_URL = "http://10.0.0.137:3000/"
+//    const val BASE_URL = "http://192.168.1.85:3000/"
     const val AUTH_LOGIN = "auth/login"
     const val AUTH_REGISTER = "auth/register"
+    const val USER = "users/{id}"
+    const val USER_IMAGE = "users/upload/{id}"
 
     //DATA STORE
     const val AUTH_PREF = "AUTH_PREF"
