2de0bacb63c7d7959ff884bf772160b5a99340c9
Christopher Tamayo <christopher.tamayocazarez@gmail.com>
2023-07-25 08:56:41 -0400

    UpdateUserUseCase was added in DI

--- app/src/main/java/com/tamayo/ecommerceapp/di/UseCaseModule.kt
+++ app/src/main/java/com/tamayo/ecommerceapp/di/UseCaseModule.kt
@@ -5,12 +5,17 @@ import com.tamayo.ecommerceapp.data.repository.dataSource.AuthRemoteDataSource
 import com.tamayo.ecommerceapp.data.repository.dataSourceImpl.AuthRemoteDataSourceImpl
 import com.tamayo.ecommerceapp.data.service.AuthService
 import com.tamayo.ecommerceapp.domain.repository.AuthRepository
+import com.tamayo.ecommerceapp.domain.repository.UserRepository
 import com.tamayo.ecommerceapp.domain.usecases.auth.AuthUseCase
 import com.tamayo.ecommerceapp.domain.usecases.auth.GetSessionDataUseCase
 import com.tamayo.ecommerceapp.domain.usecases.auth.LoginUseCase
 import com.tamayo.ecommerceapp.domain.usecases.auth.LogoutUseCase
 import com.tamayo.ecommerceapp.domain.usecases.auth.RegisterUseCase
 import com.tamayo.ecommerceapp.domain.usecases.auth.SaveSessionUseCase
+import com.tamayo.ecommerceapp.domain.usecases.auth.UpdateSessionUseCase
+import com.tamayo.ecommerceapp.domain.usecases.users.UpdateUserUseCase
+import com.tamayo.ecommerceapp.domain.usecases.users.UpdateUserWithImageUseCase
+import com.tamayo.ecommerceapp.domain.usecases.users.UsersUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,6 +32,13 @@ object UseCaseModule {
         register = RegisterUseCase(repository),
         saveSession = SaveSessionUseCase(repository),
         getSessionData = GetSessionDataUseCase(repository),
-        logout = LogoutUseCase(repository)
+        logout = LogoutUseCase(repository),
+        updateSessionUseCase = UpdateSessionUseCase(repository)
+    )
+
+    @Provides
+    fun providesUserUseCase(repository: UserRepository) = UsersUseCase(
+        updateUserUseCase = UpdateUserUseCase(repository),
+        updateUserWithImageUseCase = UpdateUserWithImageUseCase(repository)
     )
 }
